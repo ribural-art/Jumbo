50a8f3422fdf553415c1c0eb6874ce7dbf6948e1
ribural-art <riburalearnmoneyeazy@gmail.com>
2026-09-11 03:52:53 +0200

    Clarify CEO password comment in login.php
    Added a comment for clarity regarding the CEO password.

--- API/login.php
+++ API/login.php
@@ -5,7 +5,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 $name = $data['name'];
 $phone = $data['phone'];
 
-// IKI NICYO CEO PASSWORD YAWE
+// IKI NICYO CEO PASSWORD YAWE - KWITONDERA AMAZINA
 if($name === 'riburalearnmoneyeazy@gmail.com' && $phone === '0780046617'){
     echo json_encode(['isAdmin' => true]);
 } else {
